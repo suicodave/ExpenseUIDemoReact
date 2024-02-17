19b8525b6ee0a4bb518d130d595b6384bd8ac77b
Dave Suico <suico.dave@gmail.com>
2024-02-17 20:26:14 +0800

    Add routing for participant page

---
+++ src/ParticipantsPage.js
@@ -0,0 +1,28 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+function ParticipantsPage() {
+  return (
+    <Container>
+      {/* Stack the columns on mobile by making one full-width and the other half-width */}
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ParticipantsPage;
