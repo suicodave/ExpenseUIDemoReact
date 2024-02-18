89500d01ff2b77930cd8759c9d66aa47f6f42dc7
Joan <jmsinena@gmail.com>
2024-02-18 11:08:37 +0800

    SirrrrrUwu

--- src/Participants/ParticiantsList.js
+++ src/Participants/ParticiantsList.js
@@ -7,27 +7,27 @@ function ParticiantsList() {
       <ListGroup.Item>byrone joseph p daligdigan</ListGroup.Item>
       <ListGroup.Item>Chrisellen Begontes</ListGroup.Item>
       <ListGroup.Item>4</ListGroup.Item>
-      <ListGroup.Item>Rusty ANdrielov Panal</ListGroup.Item>
+      <ListGroup.Item>5</ListGroup.Item>
       <ListGroup.Item>6</ListGroup.Item>
-      <ListGroup.Item>Reynaldo G. Lauron</ListGroup.Item>
-      <ListGroup.Item>8</ListGroup.Item>
+      <ListGroup.Item>7</ListGroup.Item>
+      <ListGroup.Item>Felma Pamat</ListGroup.Item>
       <ListGroup.Item>Dodge Suico</ListGroup.Item>
       <ListGroup.Item>10</ListGroup.Item>
       <ListGroup.Item>11</ListGroup.Item>
       <ListGroup.Item>Alfred Mag-aso</ListGroup.Item>
-      <ListGroup.Item>Alter Eyas</ListGroup.Item>
+      <ListGroup.Item>13</ListGroup.Item>
       <ListGroup.Item>Francis Olano</ListGroup.Item>
-      <ListGroup.Item>Queenie Love Puebla</ListGroup.Item>
+      <ListGroup.Item>15</ListGroup.Item>
       <ListGroup.Item>16</ListGroup.Item>
       <ListGroup.Item>Khemark Ocariza</ListGroup.Item>
       <ListGroup.Item>18</ListGroup.Item>
       <ListGroup.Item>19</ListGroup.Item>
       <ListGroup.Item>20</ListGroup.Item>
-      <ListGroup.Item>21</ListGroup.Item>
+      <ListGroup.Item></ListGroup.Item>
       <ListGroup.Item>22</ListGroup.Item>
       <ListGroup.Item>Jestoni Villarta</ListGroup.Item>
       <ListGroup.Item>24</ListGroup.Item>
-      <ListGroup.Item>Jay Ann Torres</ListGroup.Item>
+      <ListGroup.Item></ListGroup.Item>
     </ListGroup>
   );
 }
