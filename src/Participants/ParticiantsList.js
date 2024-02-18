d97041deb0b15fbd9aac86227ecab9a94291aa3c
chrisellenbegontes <chrisellenbegontes@ckcm.edu.ph>
2024-02-18 11:47:38 +0800

    add participants

--- src/Participants/ParticiantsList.js
+++ src/Participants/ParticiantsList.js
@@ -5,7 +5,7 @@ function ParticiantsList() {
     <ListGroup>
       <ListGroup.Item>Dave Suico</ListGroup.Item>
       <ListGroup.Item>byrone joseph p daligdigan</ListGroup.Item>
-      <ListGroup.Item>3</ListGroup.Item>
+      <ListGroup.Item>Chrisellen Begontes</ListGroup.Item>
       <ListGroup.Item>4</ListGroup.Item>
       <ListGroup.Item>5</ListGroup.Item>
       <ListGroup.Item>6</ListGroup.Item>
